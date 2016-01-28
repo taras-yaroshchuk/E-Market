06f482d18a2e4538a26cecdc66bacd9a447608a4
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-01-28 17:15:39 +0200

    Model for products table representation was created.

---
+++ E-Market/src/main/java/com/market/model/Product.java
@@ -0,0 +1,50 @@
+package com.market.model;
+
+public class Product {
+    int id;
+    String name;
+    int price;
+    String description;
+    int amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+}
