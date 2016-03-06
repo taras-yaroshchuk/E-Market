486798a4bde042edd83275116d440919602f56a7
Artem <sidorow.artem@gmail.com>
2016-03-06 12:18:14 +0200

    Some javascript solutions with comments.

--- E-Market/src/main/webapp/WEB-INF/pages/home.jsp
+++ E-Market/src/main/webapp/WEB-INF/pages/home.jsp
@@ -1,20 +1,27 @@
 <html>
 <head>
     <title>Home Page</title>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="/resources/app/js/products.js"> </script>
+    <!-- use local copies of libraries-->
+    <script src="resources/app/vendor/jquery-2.2.1.js"></script>
+    <script src="resources/app/js/products.js"></script>
 </head>
 
 <body>
 <h1>Home Page</h1>
 <p>Hello, User</p>
-<table id="products_table" border="1" hidden="true">
+<br/>
+<table id="products_table" border="1">
+    <thead>
     <tr>
         <th>Name</th>
         <th>Price</th>
         <th>Description</th>
         <th>Amount</th>
     </tr>
+    </thead>
+    <tbody id="productsTableBody">
+
+    </tbody>
 </table>
 <button type="button" onclick="RestGet()">GET</button>
 </body>
--- E-Market/src/main/webapp/resources/app/js/products.js
+++ E-Market/src/main/webapp/resources/app/js/products.js
@@ -1,3 +1,57 @@
+//define namespace. It's not necessary, but will help in future
+var WebPage = WebPage || {};
+
+//define class
+var ProductsController = function () {
+    //this will be reference to the controller
+    var obj = {};
+
+    //private functions will look like this:
+    /**
+     * callback for successful call;
+     * @param data obtained in response
+     */
+    function onDataLoaded(data) {
+        //use '$' prefix when you use jQuery variable - it will be a bit easier to understand
+        var $tableBody = $("#productsTableBody");
+        $tableBody.empty();
+        //use native forEach - it's more fast. Or use Lodash/Underscore - util libs for arrays and objects
+        data.forEach(function (value) {
+            //ints just a bit more convenient way to use jQuery
+            var $row = $("<tr>");
+            $row.append($("<td>").text(value.name))
+                .append($("<td>").text(value.price))
+                .append($("<td>").text(value.description))
+                .append($("<td>").text(value.amount));
+            $tableBody.append($row);
+        });
+    }
+
+    /**
+     * data loading function
+     */
+    function loadData() {
+        $.get("getProducts", onDataLoaded);
+    }
+
+    //public functions will look like this
+    obj.init = function () {
+        loadData();
+    };
+
+    return obj;
+};
+
+/**
+ * Function that will executed after document loaded;
+ */
+$(function () {
+    //create element in namespace
+    WebPage.productsController = ProductsController();
+    //call public method
+    WebPage.productsController.init();
+});
+
 var RestGet = function () {
     $.ajax({
         type: 'GET',
